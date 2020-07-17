977eb0cf231d6c2a094614834245c8c7702fbaae
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-07-17 11:14:02 -0400

    added creation of unique value matrix

--- util/matrix_validation.py
+++ util/matrix_validation.py
@@ -6,3 +6,9 @@ def is_matrix_path_exist(path: str) -> bool:
         return True
 
     return False
+
+
+def is_matrix_sizes_equal(matrix1_path: str, matrix2_path) -> bool:
+    # with (open(matrix1_path), open(matrix2_path)) as (m1, m2):
+    #     print("yes")
+    return False
--- util/tests/test_validation.py
+++ util/tests/test_validation.py
@@ -1,6 +1,7 @@
 import os
 
 from util import matrix_validation as mv
+from util import matrix_creation as mc
 
 
 def create_matrix_file(path: str) -> None:
@@ -31,3 +32,15 @@ def test_validate_paths_fails() -> None:
     assert mv.is_matrix_path_exist(p2) is False
 
     os.remove(p1)
+
+
+def test_create_matrice_unique_value() -> None:
+    p = "matrice1.txt"
+    val = 1
+    size = 5
+    mc.create_unique_value_matrix(path=p, num_cols=size, num_rows=size, val=val)
+
+    with open(p) as f:
+        exp_line = [val for x in range(0, size)]
+        for line in f.readlines():
+            assert [int(x) for x in line.split()] == exp_line
