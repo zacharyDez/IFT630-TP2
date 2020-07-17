a537f6af62faded67aa125a266cc99e566c31e9d
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-07-17 17:56:08 -0400

    still fiddling around with it

---
+++ dummy.py
@@ -0,0 +1,44 @@
+from mpi4py import MPI
+import math
+
+from util.matrix_read import get_matrix_val_gen
+
+
+def divide_array(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+
+comm = MPI.COMM_WORLD
+num_process = size = comm.Get_size()  # new: gives number of ranks in comm
+rank = comm.Get_rank()
+
+# default if rank is not 0
+split_merge_data = None
+
+if rank == 0:
+    m1_data = [val for val in get_matrix_val_gen("m1.txt")]
+    m2_data = [val for val in get_matrix_val_gen("m2.txt")]
+    merge_data = [(m1_data[i], m2_data[i]) for i in range(len(m1_data))]
+
+    ave, res = divmod(len(merge_data), num_process)
+    split_merge_data = divide_array(merge_data, num_process - 1)
+
+    for i in range(len(split_merge_data)):
+        comm.send(split_merge_data[i], dest=i, tag=rank + rank * 10)
+
+else:
+
+    # Worker
+
+    # Réception de données par appel bloquant.
+    data = comm.irecv(source=0, tag=rank + rank * 10)
+
+    print(f"Process {rank} received: {data}")
--- matrix_mult_mpi.py
+++ matrix_mult_mpi.py
@@ -8,6 +8,18 @@ import util.matrix_read as mr
 import util.matrix_creation as mc
 
 
+def divide_array(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+
 def main(dest_path: str, m1_path: str, m2_path: str) -> None:
     if not mv.is_matrix_path_exist(m1_path) or not mv.is_matrix_path_exist(m2_path):
         raise ValueError(f"{m1_path} or {m2_path} does not exist.")
@@ -25,28 +37,27 @@ def main(dest_path: str, m1_path: str, m2_path: str) -> None:
 
     # defined receive buffer based on size of matrice and number of processes
     dim = mr.get_matrix_size(m1_path)
-    num_elements_per_process = int(np.ceil(dim[0]*dim[0]/size-1))
-
-    # must reference data to avoid reference before assignment in workers
-    merged_data = None
+    num_elements_per_process = 10 #int(np.ceil(dim[0] * dim[0] / size - 1))
 
     # Administrator process
     if rank == 0:
         m1_data = [val for val in mr.get_matrix_val_gen(m1_path)]
         m2_data = [val for val in mr.get_matrix_val_gen(m2_path)]
 
-        merged_data = np.array([(m1_data[i], m2_data[i]) for i in range(len(m1_data))])
+        merge_data = [(m1_data[i], m2_data[i]) for i in range(len(m1_data))]
+
+    else:
+        # must reference data to avoid reference before assignment in workers
+        merge_data = None
 
     # Worker process
     receive_buffer = np.empty(num_elements_per_process, dtype='d')
-    comm.Scatter(np.array(merged_data), receive_buffer, root=0)
+    comm.Scatter(merge_data, receive_buffer, root=0)
 
     print(f"Process {rank} received: {receive_buffer}")
 
 
 if __name__ == '__main__':
-    mc.create_unique_value_matrix("m1.txt", 5, 5, 5)
-    mc.create_unique_value_matrix("m2.txt", 5, 5, 3)
     main(sys.argv[1], sys.argv[2], sys.argv[3])
     # TODO: fix being able to call with more than two processes
-    #  Example command that works for now: mpiexec -n 2 python matrix_mult_mpi.py
+    #  Example command that works for now: mpiexec -n 2 python matrix_mult_mpi.py result.txt m1.txt m2.txt
