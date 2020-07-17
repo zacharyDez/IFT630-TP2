85662bb631a030cf8792b7e8fb8f29006be54c48
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-07-17 12:54:35 -0400

    added get_matrix_size

--- util/tests/test_read.py
+++ util/tests/test_read.py
@@ -7,7 +7,7 @@ from util import matrix_creation as mc
 def test_yield_matrices_values() -> None:
     mc.create_unique_value_matrix("m1.txt", 5, 5, 1)
 
-    actual_vals = [val for val in mr.getMatrixValGen("m1.txt")]
+    actual_vals = [val for val in mr.get_matrix_val_gen("m1.txt")]
 
     # getMatrixVal returns a float generator
     expected_vals = [float(1) for x in range(0, 5 * 5)]
@@ -15,3 +15,8 @@ def test_yield_matrices_values() -> None:
     assert actual_vals == expected_vals and len(actual_vals) == 25
 
     os.remove("m1.txt")
+
+
+def test_get_matrix_size() -> None:
+    mc.create_unique_value_matrix("m1.txt", 5, 3, 1)
+    assert mr.get_matrix_size("m1.txt") == (5, 3)
