c973c1c22dd130f1f5b2696665a9837fece80e02
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-07-17 11:38:40 -0400

    completed validation of matrice size

--- util/matrix_validation.py
+++ util/matrix_validation.py
@@ -9,6 +9,15 @@ def is_matrix_path_exist(path: str) -> bool:
 
 
 def is_matrix_sizes_equal(matrix1_path: str, matrix2_path) -> bool:
-    # with (open(matrix1_path), open(matrix2_path)) as (m1, m2):
-    #     print("yes")
-    return False
+    with open(matrix1_path) as m1, open(matrix2_path) as m2:
+        m1_vals = m1.readlines()
+        m2_vals = m2.readlines()
+
+        if len(m1_vals) != len(m2_vals):
+            return False
+
+        for i in range(0, len(m1_vals) - 1):
+            if len(m1_vals[i]) != len(m2_vals[i]):
+                return False
+
+    return True
--- util/tests/test_validation.py
+++ util/tests/test_validation.py
@@ -34,8 +34,8 @@ def test_validate_paths_fails() -> None:
     os.remove(p1)
 
 
-def test_create_matrice_unique_value() -> None:
-    p = "matrice1.txt"
+def test_create_matrix_unique_value() -> None:
+    p = "matrix1.txt"
     val = 1
     size = 5
     mc.create_unique_value_matrix(path=p, num_cols=size, num_rows=size, val=val)
@@ -44,3 +44,49 @@ def test_create_matrice_unique_value() -> None:
         exp_line = [val for x in range(0, size)]
         for line in f.readlines():
             assert [int(x) for x in line.split()] == exp_line
+
+    os.remove(p)
+
+
+def test_matrix_sizes_equal() -> None:
+    p1 = "matrix1.txt"
+    p2 = "matrix2.txt"
+    val = 1
+    size = 5
+    mc.create_unique_value_matrix(path=p1, num_cols=size, num_rows=size, val=val)
+    mc.create_unique_value_matrix(path=p2, num_cols=size, num_rows=size, val=val)
+
+    assert mv.is_matrix_sizes_equal(p1, p2) is True
+
+    os.remove(p1)
+    os.remove(p2)
+
+
+def test_matrix_cols_number_not_equal() -> None:
+    p1 = "matrix1.txt"
+    p2 = "matrix2.txt"
+    val = 1
+    size1 = 5
+    size2 = 4
+    mc.create_unique_value_matrix(path=p1, num_cols=size1, num_rows=size1, val=val)
+    mc.create_unique_value_matrix(path=p2, num_cols=size1, num_rows=size2, val=val)
+
+    assert mv.is_matrix_sizes_equal(p1, p2) is False
+
+    os.remove(p1)
+    os.remove(p2)
+
+
+def test_matrix_cols_number_not_equal() -> None:
+    p1 = "matrix1.txt"
+    p2 = "matrix2.txt"
+    val = 1
+    size1 = 5
+    size2 = 4
+    mc.create_unique_value_matrix(path=p1, num_cols=size2, num_rows=size1, val=val)
+    mc.create_unique_value_matrix(path=p2, num_cols=size1, num_rows=size1, val=val)
+
+    assert mv.is_matrix_sizes_equal(p1, p2) is False
+
+    os.remove(p1)
+    os.remove(p2)
